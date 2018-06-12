87ef4919b242d2477143c4ecf11a8625e59245f4
Sean <wb-ss400878@alibaba-inc.com>
2018-06-12 10:34:24 +0800

    小调整

--- autoconfiguration/src/main/java/io/terminus/mq/autoconfig/MQAutoConfig.java
+++ autoconfiguration/src/main/java/io/terminus/mq/autoconfig/MQAutoConfig.java
@@ -2,13 +2,12 @@
  * io.terminus
  * Copyright(c) 2012-2018 All Rights Reserved.
  */
-package io.terminus.mq.integration.autoconfig;
+package io.terminus.mq.autoconfig;
 
 import io.terminus.mq.config.MQConsumerProperties;
 import io.terminus.mq.config.MQProducerProperties;
 import io.terminus.mq.config.MQProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.Configuration;
  * @version Id:,v0.1 2018/6/12 上午10:15 sean Exp $
  * @description
  */
-@ComponentScan
 @Configuration
 @EnableConfigurationProperties({ MQProperties.class, MQProducerProperties.class, MQConsumerProperties.class })
 public class MQAutoConfig {
